3221578935a714cfe01ae1df53b8fab06c9a38c5
Jamie Barton <jamie@notrab.dev>
2023-06-27 14:21:46 +0100

    chore(grafbase): set optional type field for product

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -3,7 +3,7 @@ import { g, config } from '@grafbase/sdk'
 g.model('Product', {
   name: g.string(),
   brand: g.string(),
-  type: g.string(),
+  type: g.string().optional(),
   price: g.int().default(0)
 }).search()
 
